8ada129a0ebb7dc83cdc90843dffb8d8c3fd41d7
FalseR20 <falser.mk@gmail.com>
2024-04-23 01:56:56 +0300

    feat: add authentication

---
+++ src/main/java/dev/falser/railways_test_task/users/controllers/AuthController.java
@@ -0,0 +1,34 @@
+package dev.falser.railways_test_task.users.controllers;
+
+import dev.falser.railways_test_task.users.dto.JwtAuthenticationResponse;
+import dev.falser.railways_test_task.users.dto.SignInRequest;
+import dev.falser.railways_test_task.users.dto.SignUpRequest;
+import dev.falser.railways_test_task.users.services.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Аутентификация")
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    @Operation(summary = "Регистрация пользователя")
+    @PostMapping("/sign-up")
+    public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
+        return authenticationService.signUp(request);
+    }
+
+    @Operation(summary = "Авторизация пользователя")
+    @PostMapping("/sign-in")
+    public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
+        return authenticationService.signIn(request);
+    }
+}
---
+++ src/main/java/dev/falser/railways_test_task/users/models/Role.java
@@ -0,0 +1,6 @@
+package dev.falser.railways_test_task.users.models;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
