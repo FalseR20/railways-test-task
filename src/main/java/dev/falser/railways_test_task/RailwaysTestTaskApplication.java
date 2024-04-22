4709fa1faa67863bed2a7e32efeb0d47b7b7ad93
FalseR20 <falser.mk@gmail.com>
2024-04-22 16:19:53 +0300

    feat: init spring boot application

---
+++ src/main/java/dev/falser/railways_test_task/RailwaysTestTaskApplication.java
@@ -0,0 +1,13 @@
+package dev.falser.railways_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaysTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RailwaysTestTaskApplication.class, args);
+    }
+
+}
